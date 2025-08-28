92d495a4d186228aff4b26704a0ea9bf1ece4c3e
Zack Chaffee <zchaffee1@hawk.iit.edu>
2025-08-28 10:27:31 -0500

    Changed browser to zen

--- lua/zack/lazy/markdown.lua
+++ lua/zack/lazy/markdown.lua
@@ -1,12 +1,11 @@
 return
 {
-
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
     build = "cd app && yarn install",
     config = function ()
-      vim.g.mkdp_browser = "firefox"
+      vim.g.mkdp_browser = "zen-browser"
       vim.api.nvim_set_keymap('n', '<leader>md', '<Plug>MarkdownPreview', {noremap = true, silent = true})
     end
   }
